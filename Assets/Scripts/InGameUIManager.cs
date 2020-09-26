ba19851ce858bee9fc386377088c30ab949bb1f7
NamTaeHee1 <skaxogml777@naver.com>
2020-09-26 15:57:50 +0900

    [ADD] ESC Stop
    [ADD] ESC

---
+++ Assets/Scripts/InGameUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameUIManager : MonoBehaviour
+{
+    bool isPause = false;
+    public GameObject PausePanel;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!PausePanel.active)
+                PausePanel.SetActive(true);
+            else
+                PausePanel.SetActive(false);
+        }
+    }
+
+    public void BackToStage()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+}
