c1b1ec1497ee71938ee4d35cda0e6dccec46af3c
SungHoon <leesunghoon03@naver.com>
2020-09-26 16:13:15 +0900

    [ADD] BlockMove_Complete
    [ADD] - BlockMove_Complete

--- Assets/Scripts/BlockMove.cs
+++ Assets/Scripts/BlockMove.cs
@@ -8,14 +8,21 @@ using UnityEngine;
 public class BlockMove : MonoBehaviour
 {
     public GameObject player;
+    public GameObject firstBlock;
     public bool canInput = true;
     public bool canMove = true;
+    public bool winCheck = false;
 
-    private RaycastHit hit;
+    private RaycastHit moveHit;
+    private RaycastHit checkHit;
+
+    public int nameCnt = 1;
+    private int maxCnt = 3;
 
     void Update()
     {
         GetInput();
+        ArrayCheck(firstBlock);
     }
 
     void GetInput()
@@ -58,15 +65,15 @@ public class BlockMove : MonoBehaviour
         movePos += transform.right * 5;
         movePos += transform.up * 5;
 
-        if (Physics.Raycast(movePos, Vector3.down, out hit, 4.7f))
+        if (Physics.Raycast(movePos, Vector3.down, out moveHit, 4.7f))
         {
-            if(hit.transform.name == "block1")
+            if(moveHit.transform.name == "block1")
             {
                 canMove = false;
                 return;
             }
             else
-                Move(hit.transform.gameObject);
+                Move(moveHit.transform.gameObject);
         }
         if(canMove)
         {
@@ -90,6 +97,37 @@ public class BlockMove : MonoBehaviour
                 break;
             }
             yield return null;
+        }
+    }
+
+    void ArrayCheck(GameObject me)
+    {
+        if (nameCnt == maxCnt)
+        {
+            winCheck = true;
+            return;
+        }
+
+        nameCnt += 1;
+
+        Vector3 movePos = me.transform.position;
+        movePos += -Vector3.forward * 5;
+        movePos += Vector3.up * 5;
+
+            if (Physics.Raycast(movePos, Vector3.down, out checkHit, 4.7f))
+            {
+                if (checkHit.transform.name == "block" + nameCnt.ToString())
+                    ArrayCheck(checkHit.transform.gameObject);
+            else
+            {
+                nameCnt = 1;
+                return;
+            }
+        }
+        else
+        {
+            nameCnt = 1;
+            return;
         }
     }
 }
--- Assets/Scripts/ChainCheck.cs
+++ Assets/Scripts/ChainCheck.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class ChainCheck : MonoBehaviour
 {
+    RaycastHit hit;
+    private bool canMove;
+    private int nameCnt = 1;
+    private int maxCnt = 3;
     void Start()
+    {
+        //int i = 1;
+        //Debug.Log("block" + i.ToString());
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void ArrayCheck(GameObject me)
     {
+        if(nameCnt == maxCnt)
+        {
+            Debug.Log("Finished");
+            return;
+        }
+
+        nameCnt += 1;
         
+        Vector3 movePos = me.transform.position;
+        movePos += Vector3.right * 5;
+        movePos += Vector3.up * 5;
+
+        if (Physics.Raycast(movePos, Vector3.down, out hit, 4.7f))
+        {
+            if (hit.transform.name == "block" + nameCnt.ToString())
+                ArrayCheck(hit.transform.gameObject);
+            else
+                return;
+        }
+        return;
     }
 }
