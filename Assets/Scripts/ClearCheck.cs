a95e9cabac8e7cc82fd1c1375cdb5d11dafbde4f
SungHoon <leesunghoon03@naver.com>
2020-10-14 22:20:35 +0900

    [ADD] Seperate Block Code
    [ADD] - Seperate Block Code

--- Assets/Scripts/ClearCheck.cs
+++ Assets/Scripts/ClearCheck.cs
@@ -15,37 +15,6 @@ public class ClearCheck : MonoBehaviour
 
     void ArrayCheck(Vector3 me)
     {
-        RaycastHit checkHit;
-        Vector3 newPos = me;
-        if (nameCnt == maxCnt)
-        {
-            winCheck = true;
-            return;
-        }
-
-        nameCnt += 1;
-
-        Vector3 movePos = me;
-        movePos += -Vector3.forward * 1;
-        movePos += Vector3.up * 1;
-
-        if (Physics.Raycast(movePos, Vector3.down, out checkHit, 0.7f))
-        {
-            if (checkHit.transform.name == "block" + nameCnt.ToString())
-            {
-                movePos += Vector3.up * -1;
-                ArrayCheck(movePos);
-            }
-            else
-            {
-                nameCnt = 1;
-                return;
-            }
-        }
-        else
-        {
-            nameCnt = 1;
-            return;
-        }
+        
     }
 }
