8da209cf8699c42ce8c1d41396152c9edbd6f414
NamTaeHee1 <skaxogml777@naver.com>
2020-10-14 19:43:52 +0900

    Revert "Revert "Merge branch 'master' of https://github.com/Sunghooni/Narsha_CodeAdventure""
    This reverts commit 3e12266719e2947264d42da12a9d285cddd43f98.

---
+++ Assets/Scripts/ClearCheck.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCheck : MonoBehaviour
+{
+    private int nameCnt = 1;
+    private int maxCnt = 3;
+    public bool winCheck = false;
+
+    void Update()
+    {
+        ArrayCheck(this.transform.position);
+    }
+
+    void ArrayCheck(Vector3 me)
+    {
+        RaycastHit checkHit;
+        Vector3 newPos = me;
+        if (nameCnt == maxCnt)
+        {
+            winCheck = true;
+            return;
+        }
+
+        nameCnt += 1;
+
+        Vector3 movePos = me;
+        movePos += -Vector3.forward * 1;
+        movePos += Vector3.up * 1;
+
+        if (Physics.Raycast(movePos, Vector3.down, out checkHit, 0.7f))
+        {
+            if (checkHit.transform.name == "block" + nameCnt.ToString())
+            {
+                movePos += Vector3.up * -1;
+                ArrayCheck(movePos);
+            }
+            else
+            {
+                nameCnt = 1;
+                return;
+            }
+        }
+        else
+        {
+            nameCnt = 1;
+            return;
+        }
+    }
+}
