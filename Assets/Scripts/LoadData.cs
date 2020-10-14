faf0627d4d6d128dac9885d90cf0c3b05fa749e5
NamTaeHee1 <skaxogml777@naver.com>
2020-10-14 19:34:37 +0900

    [TEMP]

---
+++ Assets/Scripts/LoadData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LoadData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Load();
+    }
+
+    void Load()
+    {
+        string jsonString = File.ReadAllText(Application.dataPath + "/Resources/StageClear.json");
+        Debug.Log(jsonString);
+    }   
+}
