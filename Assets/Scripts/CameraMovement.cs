56d3742a9102033be25637b0b94da926c38c4123
SungHoon <leesunghoon03@naver.com>
2020-10-23 16:06:16 +0900

    [FIX] Map
    [FIX] - Map

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    private void FixedUpdate()
+    {
+        /*
+        var pos = player.transform.position;
+        gameObject.transform.position = new Vector3(pos.x, pos.y + 5.6f, pos.z - 2.5f);
+        */
+    }
+}
