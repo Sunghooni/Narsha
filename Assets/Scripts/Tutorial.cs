98a4ced4a81c518456cbcf94d6f3c959e83154d1
NamTaeHee1 <skaxogml777@naver.com>
2020-11-02 20:52:16 +0900

    [ADD] TutorialUI
    TotorialUI 추가

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,18 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    string[] TutorialText;
+    bool oneClear = false, twoClear = false, threeClear = false;
+    public Text content;
+    public GameObject W, A, S, D, Arrow, Block;
+    float CurTime = 0;
+    private void Awake()
     {
-        
+        TutorialText = new string[] { "WASD를 눌러 움직여보세요" ,
+                                                              "블럭을 밀어 옮겨보세요" ,
+                                                              "정답블럭칸에 START를 맞춰보세요!"};
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        content.text = TutorialText[0];
+        Arrow.SetActive(false);
+    }
     void Update()
     {
-        
+        ControlText();
+        if (!oneClear)
+        {
+            ClearOne();
+        }
+        if (!twoClear)
+        {
+            ClearTwo();
+        }
     }
+
+    void ControlText()
+    {
+        if (oneClear)
+        {
+            if (twoClear)
+            {
+                content.text = TutorialText[2];
+                return;
+            }
+            content.text = TutorialText[1];
+        }
+
+
+    }
+
+    void ClearOne()
+    {
+        if (!W.activeInHierarchy && !A.activeInHierarchy && !S.activeInHierarchy && !D.activeInHierarchy)
+        {
+            oneClear = true;
+            Arrow.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+            W.SetActive(false);
+        if (Input.GetKeyDown(KeyCode.A))
+            A.SetActive(false);
+        if (Input.GetKeyDown(KeyCode.S))
+            S.SetActive(false);
+        if (Input.GetKeyDown(KeyCode.D))
+            D.SetActive(false);
+
+    }
+
+    void ClearTwo()
+    {
+        if (Block.transform.position.x != -1.1516)
+        {
+            Arrow.SetActive(false);
+            twoClear = true;
+        }
+    }
+
+    void ClearThree()
+    {
+
+    }
+
 }
