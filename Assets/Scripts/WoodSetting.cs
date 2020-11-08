aa60316c460695a1a79b1f7720afa912ff5a7d29
NamTaeHee1 <skaxogml777@naver.com>
2020-11-08 20:49:41 +0900

    [Update] StageOneClear
    [Update] StageOneClear

---
+++ Assets/Scripts/WoodSetting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodSetting : MonoBehaviour
+{
+    Transform WoodTransform;
+    Rigidbody WoodRigidBody;
+
+    private void Awake()
+    {
+        WoodTransform = GetComponent<Transform>();
+        WoodRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (WoodTransform.position.y <= 1.1f)
+            WoodRigidBody.useGravity = false;
+    }
+}
