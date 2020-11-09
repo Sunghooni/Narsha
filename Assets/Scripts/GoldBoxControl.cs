9270c6b92fd81223afcdd3c5c839dac68bbcb6f1
NamTaeHee1 <skaxogml777@naver.com>
2020-11-09 12:34:15 +0900

    [ADD] StageOneClear
    [ADD] StageOneClear

---
+++ Assets/Scripts/GoldBoxControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldBoxControl : MonoBehaviour
+{
+    public GameObject GoldBoxPart;
+    bool isPlayer = false;
+
+    private void Update()
+    {
+        if(isPlayer)
+            GoldBoxPart.transform.Rotate(Vector3.up * 3.0f * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("fhuwef");
+            isPlayer = true;
+        }
+    }
+
+    private void GoldBoxAnim()
+    {
+
+    }
+}
