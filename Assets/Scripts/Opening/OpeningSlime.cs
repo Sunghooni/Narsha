fcc9f19009900555871af69248c94d1de4115818
ChanMin0334 <52943035+ChanMin0334@users.noreply.github.com>
2020-11-12 00:47:55 +0900

    [ADD]opening

---
+++ Assets/Scripts/Opening/OpeningSlime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningSlime : MonoBehaviour //todo  : 슬라임이 자동으로 이동하면서 NTNT밀기
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Opening/OpeningUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpeningUI : MonoBehaviour
+{
+    RectTransform NTNT;
+    int i = 0;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        NTNT = FindObjectOfType<OpeningUI>().GetComponent<RectTransform>();
+
+          
+        Invoke("moveUI", 0.3f);       
+    }
+
+
+    void moveUI()
+    {
+        if (i < 12)
+        {
+            Vector2 aPos = NTNT.position;
+
+            aPos.x = aPos.x -= 100;
+
+            NTNT.position = aPos;
+
+            i++;
+
+            Invoke("moveUI", 0.3f);
+        }
+    }
+}
