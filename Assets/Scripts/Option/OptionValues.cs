8b84c4acd2bdcf3013d0a79f1b36ac25345370c0
happlyin <54892429+happlyin@users.noreply.github.com>
2020-11-11 19:26:02 +0900

    [UPDATE] Option UI

--- Assets/Scripts/Option/OptionValues.cs
+++ Assets/Scripts/Option/OptionValues.cs
@@ -8,9 +8,6 @@ public class OptionValues
 
     private int _Volume = 100;
     private bool _isControlKey = true; //True : WASD, False : 방향키
-    private bool _isScreenMode = true; //True : Full, False : Window
-    private int _WindowSizeRow = 1920; //해상도 가로 크기
-    private int _WindowSizeCol = 1080; //해상도 세로 크기
 
     public int Volume
     {
@@ -22,24 +19,6 @@ public class OptionValues
     {
         set { _isControlKey = value; }
         get { return _isControlKey; }
-    }
-
-    public bool isScreenMode
-    {
-        set { _isScreenMode = value; }
-        get { return _isScreenMode; }
-    }
-
-    public int WindowSizeRow
-    {
-        set { _WindowSizeRow = value; }
-        get { return _WindowSizeRow; }
-    }
-
-    public int WindowSizeCol
-    {
-        set { _WindowSizeCol = value; }
-        get { return _WindowSizeCol; }
     }
 
     static OptionValues optionvalue = new OptionValues();
