36164d5adcddc43ef00552210bb19196e90019bd
ChanMin0334 <52943035+ChanMin0334@users.noreply.github.com>
2020-11-11 23:40:37 +0900

    [ADD]Quit button

---
+++ Assets/Scripts/Main/QuitMain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitMain : MonoBehaviour
+{
+    public void Quitclick()
+    {
+        
+          UnityEditor.EditorApplication.isPlaying = false; //출시할땐 주석처리해서 내자 ㅇㅋ?
+
+          Application.Quit(); // 어플리케이션 종료
+    }
+}
