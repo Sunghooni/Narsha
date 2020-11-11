3d27f25bbb42047bb142f95c873b8b2ae1464ada
NamTaeHee1 <skaxogml777@naver.com>
2020-11-12 00:47:19 +0900

    [UPDATE] MainAndStageSelect
    [UPDATE] - MainAndStageSelect

---
+++ Assets/Scripts/StageSelect/Stageselector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stageselector : MonoBehaviour
+{
+    public void tutorial()
+    {
+        LoadingScene.LoadScene("tutorial");
+    }
+    public void stage1click()
+    {
+        LoadingScene.LoadScene("Stage1");
+    }
+}
