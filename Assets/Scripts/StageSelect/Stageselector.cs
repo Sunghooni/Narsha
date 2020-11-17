269bea264f164c668b2507e28f742f937a5e64d1
chanmin0334 <chanmin0334@naver.com>
2020-11-17 09:28:48 +0900

    [FIX]Stage sel

--- Assets/Scripts/StageSelect/Stageselector.cs
+++ Assets/Scripts/StageSelect/Stageselector.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StageSelector : MonoBehaviour
+public class Stageselector : MonoBehaviour
 {
     GameObject audio;
 
